a4a7fcf5f759628aed9068876ad4504eb831d486
panglin123 <panglin@hrbmu.edu.cn>
2025-12-29 16:35:51 +0800

    Delete AddModuleScore.R

--- AddModuleScore.R
+++
@@ -1,9 +0,0 @@
-# AddModuleScore
-#' @param object seurat_obj: a Seurat object of malignant cell
-#' @param features GeneSets: list of gene sets
-#' @param name names(GeneSets)
-
-library(Seurat)
-score <- AddModuleScore(object = seurat_obj, features = GeneSets, name = names(GeneSets))
-
-
